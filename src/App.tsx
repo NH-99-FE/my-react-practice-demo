3003aeaaefa20533045cfba8fd90f6115ec0fa0a
NH-99-FE <lianglh_only@163.com>
2025-06-16 17:05:41 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <>
+      <div className="text-bold text-xl text-gray-800">APP</div>
+      <div className="text-bold text-xl text-gray-800">yusxdgbxyu</div>
+    </>
+  )
+}
+
+export default App

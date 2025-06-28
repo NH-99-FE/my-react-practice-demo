acdf5c508c1799a91e3fa29f256137037292c3f1
NH-99-FE <lianglh_only@163.com>
2025-06-28 20:24:54 +0800

    feat: build分包

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tailwindcss from '@tailwindcss/vite'
+import { visualizer } from 'rollup-plugin-visualizer'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -12,5 +13,23 @@ export default defineConfig({
       },
     },
   },
-  plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      output: {
+        //分包
+        manualChunks: {
+          'react-vendor': ['react', 'react-router'],
+          'antd-vendor': ['antd'],
+          'echarts-vendor': ['echarts'],
+          'user-vendor': [
+            './src/pages/user',
+            './src/pages/dept',
+            './src/pages/role',
+            './src/pages/menu',
+          ],
+        },
+      },
+    },
+  },
+  plugins: [react(), tailwindcss(), visualizer({ open: true })],
 })
